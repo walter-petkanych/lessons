5c1169bcd4ca09ca7ae53ecf34aaa71c88b87513
walter-petkanych <silwerfild@gmail.com>
2021-04-28 22:04:07 +0300

    modified repositories

---
+++ sample_app/app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelper
+
+# Возвращает полный заголовок на основе заголовка страницы.
+  def full_title(page_title = '')
+      base_title = "Ruby on Rails Tutorial Sample App"
+      if page_title.empty?
+          base_title
+      else
+          page_title + " | " + base_title  
+      end
+  end
+
+end 
---
+++ sample_app/test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+ 
+  test "Layout Links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", '/', count: 2
+    assert_select "a[href=?]", 'help'
+    assert_select "a[href=?]", 'about'
+    assert_select "a[href=?]", 'contact'
+  end
+end
